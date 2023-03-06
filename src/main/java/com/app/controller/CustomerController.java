1c88624ee97900e77ef0294a2ccd1606a4178536
SK Ikbal <sk.ikbal@STLERI.COM>
2023-03-06 15:21:51 +0530

    myproject_with_mysql

---
+++ src/main/java/com/app/controller/CustomerController.java
@@ -0,0 +1,43 @@
+package com.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.app.entity.Customer;
+import com.app.repository.CustomerRepository;
+
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	@Autowired
+	private CustomerRepository customerRepo;
+	
+	@GetMapping("/registration")
+	public String viewRegistration(Model model) {
+		Customer c= new Customer();
+		model.addAttribute("customer", c);
+		return "signup";
+	}
+	
+	@PostMapping("/save")
+	public String getDataFromRegistration(@ModelAttribute("customer")Customer customer,Model model) {
+		
+		if (customerRepo.existsByEmail(customer.getEmail())) {
+			model.addAttribute("message", "Data Exist in this Email Id !! try new One");
+			return "signup";
+			
+		}
+		
+		customerRepo.save(customer);
+		model.addAttribute("customer", customer);
+		return "successfull";
+	}
+
+}
---
+++ src/main/java/com/app/entity/Customer.java
@@ -0,0 +1,60 @@
+package com.app.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="customers")
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	private String email;
+	private String username;
+	private String password;
+	private String gender;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	
+
+}
